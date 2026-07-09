534c14436877f6636cc9bff464c3acdda8bd3c25
NaufalYogaPratama <naufaltino2113@gmail.com>
2026-07-09 13:55:05 +0700

    fix: aligning entity with flyway schema

--- src/main/java/com/indivaragroup/ageninlite/repository/auth/RefreshTokenRepository.java
+++ src/main/java/com/indivaragroup/ageninlite/repository/auth/RefreshTokenRepository.java
@@ -9,5 +9,5 @@ import java.util.UUID;
 
 @Repository
 public interface RefreshTokenRepository extends JpaRepository<AuthRefreshToken, UUID> {
-    Optional<AuthRefreshToken> findByToken(String token);
+    Optional<AuthRefreshToken> findByTokenId(String tokenId);
 }
