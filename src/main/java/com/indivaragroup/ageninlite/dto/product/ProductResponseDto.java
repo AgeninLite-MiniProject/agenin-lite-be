d11f5ea620a40d27ca2b2ef33e80c9968d6ac18e
Ediw7 <ediwicoro26@gmail.com>
2026-07-11 13:05:12 +0700

    feat(product-admin): add CORS and map financial fields to ProductResponseDto

--- src/main/java/com/indivaragroup/ageninlite/dto/product/ProductResponseDto.java
+++ src/main/java/com/indivaragroup/ageninlite/dto/product/ProductResponseDto.java
@@ -3,12 +3,17 @@ package com.indivaragroup.ageninlite.dto.product;
 import lombok.Builder;
 import lombok.Data;
 import java.util.UUID;
+import java.math.BigDecimal;
 
 @Data
 @Builder
 public class ProductResponseDto {
     private UUID product_id;
     private String product_name;
+    private BigDecimal cost_price;
+    private BigDecimal selling_price;
+    private BigDecimal agent_fee;
+    private BigDecimal super_agent_fee;
     private String product_status;
     private String message;
 }
