bc90b166a4ea2eb769c2b1a5663984706659bd3c
Ediw7 <ediwicoro26@gmail.com>
2026-07-13 13:53:19 +0700

    feat(product-agent): add active product list endpoint for agents

--- src/main/java/com/indivaragroup/ageninlite/repository/product/ProductRepository.java
+++ src/main/java/com/indivaragroup/ageninlite/repository/product/ProductRepository.java
@@ -4,9 +4,11 @@ import com.indivaragroup.ageninlite.entity.MstProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ProductRepository extends JpaRepository<MstProduct, UUID> {
     long countByProductStatus(String productStatus);
+    List<MstProduct> findByProductStatus(String productStatus);
 }
